6c93df8baedf900f12000a842b31000878e14e73
kamleshkaparvena <kamleshkaparvena3570@gmail.com>
2024-04-01 16:59:12 +0530

    hero_part_done_navg

---
+++ components/MainSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroPage from './Hero'
+
+const MainSection = () => {
+  return (
+      <main className='overflow-hidden'>
+          <HeroPage/>
+      </main>
+  )
+}
+
+export default MainSection
